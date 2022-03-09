077e4328e9d8c92a38796ff8236b8a1bc682e431
UlaşDK <udk97@hotmail.com>
2022-03-09 19:06:22 +0300

    road

--- Controllers/photoControllers.js
+++ Controllers/photoControllers.js
@@ -16,19 +16,19 @@ exports.getPhoto = async (req, res) => {
 };
 
 exports.createPhoto = async (req, res) => {
-  const uploadDir = 'public/uploads';
+  const uploadDir = 'public\\uploads';
 
   if (!fs.existsSync(uploadDir)) {
     fs.mkdirSync(uploadDir);
   }
 
   let uploadeImage = req.files.image;
-  let uploadPath = __dirname + '/../public/uploads/' + uploadeImage.name;
+  let uploadPath = __dirname + '\\..\\public\\uploads\\' + uploadeImage.name;
 
   uploadeImage.mv(uploadPath, async () => {
     await Photo.create({
       ...req.body,
-      image: '/uploads/' + uploadeImage.name,
+      image: '\\uploads\\' + uploadeImage.name,
     });
     res.redirect('/index');
   });
@@ -39,12 +39,12 @@ exports.updatePhoto = async (req, res) => {
   photo.title = req.body.title;
   photo.description = req.body.description;
   photo.save();
-  res.redirect(`/photos/${req.params.id}`);
+  res.redirect(`\\photos\\${req.params.id}`);
 };
 
 exports.deletePhoto = async (req, res) => {
   const photo = await Photo.findOne({ _id: req.params.id });
-  let deleteImage = __dirname + '/../public/' + photo.image;
+  let deleteImage = __dirname + '\\..\\public\\' + photo.image;
   fs.unlinkSync(deleteImage);
   await Photo.findByIdAndRemove(req.params.id);
   res.redirect('/index');
